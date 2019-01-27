e5c3866f3f3eae1b4d346b64254d4153d2cbee42
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-01-27 11:11:35 +0100

    Fix formatting to LLVM standard

--- heating_sensor/heating_sensor.ino
+++ heating_sensor/heating_sensor.ino
@@ -1,11 +1,11 @@
 
+#include <DallasTemperature.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include <OneWire.h>
 #include <PubSubClient.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
 
 #define wifi_ssid "ssid"
 #define wifi_password "password"
@@ -21,10 +21,8 @@
 OneWire ds(D4);
 DallasTemperature sensors(&ds);
 
-byte flowInTher[8] = { 
-  0x28, 0xE9, 0x2E, 0xC5, 0x6, 0x0, 0x0, 0x92 }; //10
-byte flowOutTher[8] = { 
-  0x28, 0x23, 0xB0, 0xC4, 0x6, 0x0, 0x0, 0x58 }; //14
+byte flowInTher[8] = {0x28, 0xE9, 0x2E, 0xC5, 0x6, 0x0, 0x0, 0x92};  // 10
+byte flowOutTher[8] = {0x28, 0x23, 0xB0, 0xC4, 0x6, 0x0, 0x0, 0x58}; // 14
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -34,11 +32,10 @@ ESP8266HTTPUpdateServer httpUpdater;
 
 unsigned long oldmillis = 0;
 
-//Something to hold the float as we string them
+// Something to hold the float as we string them
 
-
-
-//connessione WiFi ---------------------------------------------------------------------------|
+// connessione WiFi
+// ---------------------------------------------------------------------------|
 void setup_wifi() {
   delay(10);
   Serial.println("///////////////// - WiFi - /////////////////");
@@ -57,7 +54,8 @@ void setup_wifi() {
   Serial.println("");
 }
 
-//connessione MQTT ---------------------------------------------------------------------------|
+// connessione MQTT
+// ---------------------------------------------------------------------------|
 void reconnect() {
   unsigned long disconnectMillis = millis();
   while (!client.connected()) {
@@ -65,7 +63,7 @@ void reconnect() {
     Serial.println("Connessione...");
 
     if (client.connect(mqtt_id)) {
-      //digitalWrite(D2,HIGH);
+      // digitalWrite(D2,HIGH);
       Serial.println("Connesso");
       Serial.println("////////////////////////////////////////////");
     } else {
@@ -80,38 +78,37 @@ void reconnect() {
 void calculateTemp(byte *sensor) {
   ds.reset();
   ds.select(sensor);
-  ds.write(0x44, 0);        // start conversion, without parasite power
+  ds.write(0x44, 0); // start conversion, without parasite power
   delay(250);
 }
 
-float readTemp(byte *sensor){
+float readTemp(byte *sensor) {
   int i;
   float result;
   byte data[12];
-  
+
   ds.reset();
   ds.select(sensor);
-  ds.write(0xBE);          // read temperature
+  ds.write(0xBE); // read temperature
   delay(250);
-  
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-   data[i] = ds.read();
+
+  for (i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
   }
-  
-  if (OneWire::crc8(data, 8) != data[8]){   //Check CRC
-    return (250); //If temperature read is wrong, return ludicrously high temp
+
+  if (OneWire::crc8(data, 8) != data[8]) { // Check CRC
+    return (250); // If temperature read is wrong, return ludicrously high temp
   }
-  
+
   //////Code from library example sketch
   int16_t raw = (data[1] << 8) | data[0];
   return ((float)raw / 16.0);
-
 }
 
 void setup() {
   Serial.begin(BAUDRATE);
-  
-  //Set sensor resolution
+
+  // Set sensor resolution
   sensors.begin();
   sensors.setResolution(flowInTher, 12);
   sensors.setResolution(flowOutTher, 12);
@@ -135,35 +132,34 @@ void loop() {
   httpServer.handleClient();
 
   if (millis() / 1000 > oldmillis + 10) {
-  	// This will overflow every 40-ish days
-  	// but we don't care because it will never
-  	// be on for more than an hour or two
-  	Serial.println("Calculate temperature loop");
+    // This will overflow every 40-ish days
+    // but we don't care because it will never
+    // be on for more than an hour or two
+    Serial.println("Calculate temperature loop");
     sensors.requestTemperatures();
 
-    for (i = 0; i < 15; i++){
+    for (i = 0; i < 15; i++) {
       delay(100);
     }
 
     char outstring[15];
 
-    flowInRead=readTemp(flowInTher);
+    flowInRead = readTemp(flowInTher);
     if (flowInRead != 250) {
       Serial.print("Flow in: ");
       Serial.println(flowInRead);
-      dtostrf(flowInRead,6,4, outstring);
+      dtostrf(flowInRead, 6, 4, outstring);
       client.publish(topic_in, outstring);
     }
 
-    flowOutRead=readTemp(flowOutTher);
+    flowOutRead = readTemp(flowOutTher);
     if (flowOutRead != 250) {
       Serial.print("Flow out: ");
       Serial.println(flowOutRead);
-      dtostrf(flowOutRead,6,4, outstring);
+      dtostrf(flowOutRead, 6, 4, outstring);
       client.publish(topic_out, outstring);
     }
-    oldmillis = millis()/1000;
+    oldmillis = millis() / 1000;
     Serial.println("Out of the loop");
   }
-
 }
