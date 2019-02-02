9bb7b190e02d4ff81d2ac9916829d02d2a06360c
Federico Tabbò <federico@tabbo.it>
2019-02-02 19:13:17 +0100

    Add more sensors

--- heating_sensor/heating_sensor.ino
+++ heating_sensor/heating_sensor.ino
@@ -1,5 +1,4 @@
-
-#include <DallasTemperature.h>
+#include <ArduinoJson.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
@@ -11,18 +10,22 @@
 #define wifi_password "password"
 
 #define mqtt_server "192.168.1.2"
-#define mqtt_id "heating_flow_sensor"
+#define mqtt_id "heating_flow_sensor_new"
 
-#define topic_in "roncello/riscaldamento/flowin"
-#define topic_out "roncello/riscaldamento/flowout"
-
-#define BAUDRATE 9600
+#define BAUDRATE 115200
 
 OneWire ds(D4);
-DallasTemperature sensors(&ds);
 
-byte flowInTher[8] = {0x28, 0xE9, 0x2E, 0xC5, 0x6, 0x0, 0x0, 0x92};  // 10
-byte flowOutTher[8] = {0x28, 0x23, 0xB0, 0xC4, 0x6, 0x0, 0x0, 0x58}; // 14
+byte sensorlist[10][8] = {{0x28, 0xFF, 0xFD, 0x09, 0x60, 0x18, 0x03, 0x4C},
+                          {0x10, 0x44, 0xBA, 0x59, 0x03, 0x08, 0x00, 0x79},
+                          {0x10, 0x5E, 0x84, 0x59, 0x03, 0x08, 0x00, 0xCB},
+                          {0x10, 0x0E, 0xE5, 0x59, 0x03, 0x08, 0x00, 0x39},
+                          {0x28, 0xFF, 0x7B, 0x0A, 0x60, 0x18, 0x03, 0x93},
+                          {0x28, 0xFF, 0x93, 0x0D, 0x60, 0x18, 0x03, 0xF1},
+                          {0x28, 0xFF, 0x41, 0x06, 0x60, 0x18, 0x03, 0xB8},
+                          {0x28, 0xFF, 0xAA, 0x0D, 0x60, 0x18, 0x03, 0x86},
+                          {0x10, 0x21, 0xD3, 0x59, 0x03, 0x08, 0x00, 0xB3},
+                          {0x28, 0xFF, 0xEE, 0x20, 0x60, 0x18, 0x03, 0x9D}};
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -66,6 +69,7 @@ void reconnect() {
       // digitalWrite(D2,HIGH);
       Serial.println("Connesso");
       Serial.println("////////////////////////////////////////////");
+      client.publish("/riscaldamento/tubi", "READY");
     } else {
       Serial.print("Connessione fallita, rc=");
       Serial.println(client.state());
@@ -79,21 +83,25 @@ void calculateTemp(byte *sensor) {
   ds.reset();
   ds.select(sensor);
   ds.write(0x44, 0); // start conversion, without parasite power
-  delay(250);
+  client.loop();
+  for (int i = 0; i < 10; i++) {
+    delay(100);
+  }
 }
 
 float readTemp(byte *sensor) {
   int i;
   float result;
   byte data[12];
+  byte type_s;
 
   ds.reset();
   ds.select(sensor);
   ds.write(0xBE); // read temperature
-  delay(250);
 
   for (i = 0; i < 9; i++) { // we need 9 bytes
     data[i] = ds.read();
+    Serial.print(data[i]);
   }
 
   if (OneWire::crc8(data, 8) != data[8]) { // Check CRC
@@ -102,16 +110,28 @@ float readTemp(byte *sensor) {
 
   //////Code from library example sketch
   int16_t raw = (data[1] << 8) | data[0];
+  if (sensor[0] == 0x10) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00)
+      raw = raw & ~7; // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20)
+      raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40)
+      raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
   return ((float)raw / 16.0);
 }
 
 void setup() {
   Serial.begin(BAUDRATE);
-
-  // Set sensor resolution
-  sensors.begin();
-  sensors.setResolution(flowInTher, 12);
-  sensors.setResolution(flowOutTher, 12);
 
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -121,10 +141,6 @@ void setup() {
 }
 
 void loop() {
-  float flowInRead;
-  float flowOutRead;
-  char outstring[15];
-  int i;
   if (!client.connected()) {
     reconnect();
   }
@@ -136,30 +152,27 @@ void loop() {
     // but we don't care because it will never
     // be on for more than an hour or two
     Serial.println("Calculate temperature loop");
-    sensors.requestTemperatures();
 
-    for (i = 0; i < 15; i++) {
-      delay(100);
+    for (int i = 0; i < 10; i++) {
+      calculateTemp(sensorlist[i]);
     }
 
-    char outstring[15];
-
-    flowInRead = readTemp(flowInTher);
-    if (flowInRead != 250) {
-      Serial.print("Flow in: ");
-      Serial.println(flowInRead);
-      dtostrf(flowInRead, 6, 4, outstring);
-      client.publish(topic_in, outstring);
+    for (int i = 0; i < 10; i++) {
+      float reading;
+      char outstring[15];
+      reading = readTemp(sensorlist[i]);
+      if (reading != 250) {
+        Serial.print("Flow in: ");
+        Serial.println(reading);
+        dtostrf(reading, 6, 4, outstring);
+        String topic = "/riscaldamento/tubi/";
+        topic = topic + String(i);
+        Serial.println(topic);
+        client.publish(String(topic).c_str(), String(outstring).c_str());
+        delay(100);
+      }
     }
 
-    flowOutRead = readTemp(flowOutTher);
-    if (flowOutRead != 250) {
-      Serial.print("Flow out: ");
-      Serial.println(flowOutRead);
-      dtostrf(flowOutRead, 6, 4, outstring);
-      client.publish(topic_out, outstring);
-    }
     oldmillis = millis() / 1000;
-    Serial.println("Out of the loop");
   }
 }
