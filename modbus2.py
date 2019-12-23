641c9aadc4f0cff9af9e7cc0b2b746f278dccb0e
Federico Tabbò <federico@tabbo.it>
2019-12-23 19:12:56 +0100

    Rewrite modbus reader

--- modbus2.py
+++ modbus2.py
@@ -1,33 +1,45 @@
-#!/usr/bin/python2
-import struct
-import pymodbus.client.sync
-import binascii
+#!/usr/bin/python3
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.client.sync import ModbusSerialClient
 import time
-import sys
-import requests 
- 
-def main() :
-    cl = pymodbus.client.sync.ModbusSerialClient('rtu', port='/dev/ttyUSB0', baudrate=38400, parity='N',stopbits=2, timeout=0.2)
-    respa = cl.read_input_registers(12,6, unit=1)
-    respb = cl.read_input_registers(52,2, unit=1)
-    try:
-        wph1 = str(int(struct.unpack('>f',struct.pack('>HH',*[respa.registers[0],respa.registers[1]]))[0]))
-        wph2 = str(int(struct.unpack('>f',struct.pack('>HH',*[respa.registers[2],respa.registers[3]]))[0]))
-        wph3 = str(int(struct.unpack('>f',struct.pack('>HH',*[respa.registers[4],respa.registers[5]]))[0]))
-        wtot = str(int(struct.unpack('>f',struct.pack('>HH',*[respb.registers[0],respb.registers[1]]))[0]))
-
-        requests.request("put", "http://192.168.1.2/emoncms/input/post.json?node=1&csv="+wph1+","+wph2+","+wph3+","+wtot+"&apikey=5a8504496b87a071442b5d2642123fe9")
+from requests import get
+import json
 
 
-    except:
-       pass
+def main(run):
+    cl = ModbusSerialClient('rtu', port='/dev/ttyMODBUS', baudrate=38400, parity='N', stopbits=2, timeout=1)
+    resp = [cl.read_input_registers(0x000C, 6, unit=1),
+            cl.read_input_registers(0x0034, 2, unit=1)]
 
- 
-if __name__ == '__main__' :
+    if run == 0:
+        resp.append(cl.read_input_registers(0x0048, 8, unit=1))
+
+    decoder = [BinaryPayloadDecoder.fromRegisters(x.registers, byteorder=Endian.Big, wordorder=Endian.Big) for x in resp]
+
+    data = {
+        "wph1": decoder[0].decode_32bit_float(),
+        "wph2": decoder[0].decode_32bit_float(),
+        "wph3": decoder[0].decode_32bit_float(),
+        "wtot": decoder[1].decode_32bit_float()
+    }
+
+    if run == 0:
+        data["inwh"] = decoder[2].decode_32bit_float() * 1000
+        data["outwh"] = decoder[2].decode_32bit_float() * 1000
+        data["invarh"] = decoder[2].decode_32bit_float() * 1000
+        data["outvarh"] = decoder[2].decode_32bit_float() * 1000
+
+    r = get("http://192.168.1.2/emoncms/input/post?node=eastroncasa&fulljson=" + json.dumps(data) + "&apikey=5a8504496b87a071442b5d2642123fe9")
+    return r
+
+
+if __name__ == '__main__':
     startup = time.time()
     run = 0
     while time.time() < (startup + 55):
-        if time.time() > startup + (run*10) - 0.005:
+        if time.time() > startup + (run * 10) - 0.005:
+            web = main(run)
+            assert web.ok
             run = run + 1
-            main()
         time.sleep(0.1)
