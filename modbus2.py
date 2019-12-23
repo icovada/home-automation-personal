53205286ffa855f515a6b00f470ee5925c7822d1
Federico Tabbò <federico@tabbo.it>
2019-12-23 21:16:26 +0100

    Add APScheduler

--- modbus2.py
+++ modbus2.py
@@ -2,21 +2,24 @@
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.client.sync import ModbusSerialClient
-import time
+from time import sleep
 from influxdb import InfluxDBClient
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
 
 client = InfluxDBClient(host='influxdb', port=8086)
 client.switch_database('eastron')
 
+COUNTER = 0
 
-def main(run):
+def modquery():
     cl = ModbusSerialClient('rtu', port='/dev/ttyMODBUS',
                             baudrate=38400, parity='N', stopbits=2, timeout=1)
     resp = [cl.read_input_registers(0x000C, 6, unit=1),
             cl.read_input_registers(0x0034, 2, unit=1)]
 
-    if run == 0:
-        resp.append(cl.read_input_registers(0x0048, 8, unit=1))
+    resp.append(cl.read_input_registers(0x0048, 8, unit=1))
 
     decoder = [BinaryPayloadDecoder.fromRegisters(
         x.registers, byteorder=Endian.Big, wordorder=Endian.Big) for x in resp]
@@ -61,63 +64,60 @@ def main(run):
             "fields": {
                 "value": decoder[1].decode_32bit_float()
             }
+        },
+        {
+            "measurement": "Watth",
+            "tags": {
+                "entity": "Eastron casa",
+                "direction": "in"
+            },
+            "fields": {
+                "value": decoder[2].decode_32bit_float() * 1000
+            }
+        },
+        {
+            "measurement": "Watth",
+            "tags": {
+                "entity": "Eastron casa",
+                "direction": "out"
+            },
+            "fields": {
+                "value": decoder[2].decode_32bit_float() * 1000
+            }
+        },
+        {
+            "measurement": "Varh",
+            "tags": {
+                "entity": "Eastron casa",
+                "direction": "in"
+            },
+            "fields": {
+                "value": decoder[2].decode_32bit_float() * 1000
+            }
+        },
+        {
+            "measurement": "Varh",
+            "tags": {
+                "entity": "Eastron casa",
+                "direction": "out"
+            },
+            "fields": {
+                "value": decoder[2].decode_32bit_float() * 1000
+            }
         }]
 
-    if run == 0:
-        data.append({
-            "measurement": "Watth",
-            "tags": {
-                "entity": "Eastron casa",
-                "direction": "in"
-            },
-            "fields": {
-                "value": decoder[2].decode_32bit_float() * 1000
-            }
-        })
-
-        data.append({
-            "measurement": "Watth",
-            "tags": {
-                "entity": "Eastron casa",
-                "direction": "out"
-            },
-            "fields": {
-                "value": decoder[2].decode_32bit_float() * 1000
-            }
-        })
-
-        data.append({
-            "measurement": "Varh",
-            "tags": {
-                "entity": "Eastron casa",
-                "direction": "in"
-            },
-            "fields": {
-                "value": decoder[2].decode_32bit_float() * 1000
-            }
-        })
-
-        data.append({
-            "measurement": "Varh",
-            "tags": {
-                "entity": "Eastron casa",
-                "direction": "out"
-            },
-            "fields": {
-                "value": decoder[2].decode_32bit_float() * 1000
-            }
-        })
-
     r = client.write_points(data)
+
+
     return r
 
 
+
 if __name__ == '__main__':
-    startup = time.time()
-    run = 0
-    while time.time() < (startup + 55):
-        if time.time() > startup + (run * 10) - 0.005:
-            web = main(run)
-            assert web
-            run = run + 1
-        time.sleep(0.1)
+    sched = BackgroundScheduler()
+    sched.start()
+
+    sched.add_job(modquery, trigger='cron', second = '*/2')
+
+    while True:
+        sleep(1)
