ce1335e4c5acf49594d0474f79b1d71b7bbc4fa2
Federico Tabbò <federico.tabbo@networktocode.com>
2025-11-26 21:58:31 +0100

    try to fix

--- tende_est/tende_est.ino
+++ tende_est/tende_est.ino
@@ -27,7 +27,6 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 #define MQTT_HUMAN_NAME "Tende Est"
 
 #include <math.h>
-#include <EEPROM.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
@@ -52,8 +51,9 @@ public:
   int timeDown = -1;
   HACover *tenda;
   unsigned long startMillis = 0;
-  unsigned long stopMillis = 0;
+  unsigned long stopMillis = -1;
   unsigned long lastUpdate = 0;
+  int startPosition = 0;
 
   TendaManager(int pinUp, int pinDown, int timeUp, int timeDown, HACover *tenda)
       : pinUp(pinUp),
@@ -71,62 +71,84 @@ public:
   void close()
   {
     // go up
-    stop(false);
-    tenda->setCurrentState(HACover::CoverState::StateOpening);
+    Serial.println("Close");
+    stop(true, false);
+    tenda->setState(HACover::CoverState::StateClosing);
     startMillis = millis();
+    startPosition = tenda->getCurrentPosition();
     digitalWrite(pinUp, true);
   }
 
   void open()
   {
     // go down
-    stop(false);
-    tenda->setCurrentState(HACover::CoverState::StateClosing);
+    Serial.println("Open");
+    stop(true, false);
+    tenda->setState(HACover::CoverState::StateOpening);
     startMillis = millis();
+    startPosition = tenda->getCurrentPosition();
     digitalWrite(pinDown, true);
   }
 
-  void stop(bool send = true)
+  void stop(bool simple = false, bool send = true)
   {
+    Serial.println("void stop");
+    if (getPosition() == 100)
+    {
+      tenda->setState(HACover::CoverState::StateOpen);
+    }
     if (send)
     {
       if (tenda->getCurrentState() == HACover::CoverState::StateClosing)
       {
-        tenda->setCurrentState(HACover::CoverState::StateClosed);
+        tenda->setState(HACover::CoverState::StateClosed);
       }
       else if (tenda->getCurrentState() == HACover::CoverState::StateOpening)
       {
-        tenda->setCurrentState(HACover::CoverState::StateOpen);
+        if (getPosition() == 100)
+        {
+          tenda->setState(HACover::CoverState::StateOpen);
+        }
+        else
+        {
+          tenda->setState(HACover::CoverState::StateStopped);
+        }
       }
-      else
-      {
-        tenda->setCurrentState(HACover::CoverState::StateStopped);
-      }
+      lastUpdate = millis();
     }
+    startPosition = getPosition();
     digitalWrite(pinDown, false);
     digitalWrite(pinUp, false);
-    lastUpdate = millis();
-    stopMillis = -1;
-    sendPosition();
+
+    if (!simple)
+    {
+      Serial.println("void stop reset");
+      stopMillis = -1;
+      sendPosition();
+    }
   }
 
   void check()
   {
     if (tenda->getCurrentState() == HACover::CoverState::StateOpening || tenda->getCurrentState() == HACover::CoverState::StateClosing)
     {
-      if (millis() >= stopMillis)
+      if (stopMillis == -1)
       {
-        stop();
-      } else if (millis() == -1){
-        if ((millis() - 35000) > startMillis ){
+        if (millis() - startMillis > 40000)
+        {
           // it had enough
+          Serial.println("Stop end run");
           stop();
           return;
         }
       }
+      else if (millis() >= stopMillis)
+      {
+        Serial.println("Stop reached target");
+        stop();
+      }
       sendPosition();
     }
-    
   }
 
   void setPosition(int position)
@@ -146,22 +168,31 @@ public:
     }
   }
 
+  int getPosition()
+  {
+    if (tenda->getCurrentState() == HACover::CoverState::StateOpening)
+    {
+      // Opening = moving up (retracting) = increasing position toward 100
+      return min(int((millis() - startMillis) * 100 / (timeUp * 1000)), 100);
+    }
+    else if (tenda->getCurrentState() == HACover::CoverState::StateClosing)
+    {
+      // Closing = moving down (extending) = decreasing position toward 0
+      return max(int((millis() - startMillis) * 100 / (timeDown * 1000)), 0);
+    }
+    else
+    {
+      return -1;
+    }
+  }
+
   void sendPosition()
   {
+    int elapsed = -1;
     if (millis() - 1000 > lastUpdate)
     {
-      int curPosition = tenda->getCurrentPosition();
-      int delta = 0;
-      if (tenda->getCurrentState() == HACover::CoverState::StateOpening)
-      {
-        delta = (curPosition - (millis() - startMillis) / ((timeUp * 1000) / 100));
-        tenda->setPosition(tenda->getCurrentPosition() - delta);
-      }
-      else
-      {
-        delta = (curPosition - (millis() - startMillis) / ((timeDown * 1000) / 100));
-        tenda->setPosition(tenda->getCurrentPosition() + delta);
-      }
+      int position = getPosition();
+      tenda->setPosition(position);
       lastUpdate = millis();
     }
   }
@@ -173,17 +204,59 @@ HACover ha_sud("tenda_est_sud", HACover::Features::PositionFeature);
 TendaManager nord(D1, D2, 35, 30, &ha_nord);
 TendaManager sud(D6, D7, 35, 30, &ha_sud);
 
+void onCommand(HACover::CoverCommand cmd, HACover *sender)
+{
+  if (cmd == HACover::CoverCommand::CommandClose)
+  {
+    Serial.println("Received Close");
+    if (nord.tenda == sender)
+    {
+      nord.close();
+    }
+    else if (sud.tenda == sender)
+    {
+      sud.close();
+    }
+  }
+  else if (cmd == HACover::CoverCommand::CommandOpen)
+  {
+    Serial.println("Received Open");
+    if (nord.tenda == sender)
+    {
+      nord.open();
+    }
+    else if (sud.tenda == sender)
+    {
+      sud.open();
+    }
+  }
+  else if (cmd == HACover::CoverCommand::CommandStop)
+  {
+    Serial.println("Received Stop");
+    if (nord.tenda == sender)
+    {
+      nord.stop();
+    }
+    else if (sud.tenda == sender)
+    {
+      sud.stop();
+    }
+  }
+}
+
 // setup
 // --------------------------------------------------------------------------------------|
 void setup()
 {
   Serial.begin(115200);
 
-  ha_nord.setDeviceClass("awning");
+  ha_nord.setDeviceClass("blind");
   ha_nord.setName("Tenda Est Nord");
+  ha_nord.onCommand(onCommand);
 
-  ha_sud.setDeviceClass("awning");
+  ha_sud.setDeviceClass("blind");
   ha_sud.setName("Tenda Est Sud");
+  ha_sud.onCommand(onCommand);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_password);
@@ -200,6 +273,16 @@ void setup()
   device.enableLastWill();
 
   mqtt.begin("mqtt.in.tabbo.it");
+
+  // Wait for MQTT connection and set initial positions
+  while (!mqtt.isConnected())
+  {
+    mqtt.loop();
+    delay(100);
+  }
+
+  ha_nord.setPosition(0);
+  ha_sud.setPosition(0);
 
   httpUpdater.setup(&httpServer);
   httpServer.begin();
@@ -218,5 +301,4 @@ void loop()
 
   nord.check();
   sud.check();
-
 }
